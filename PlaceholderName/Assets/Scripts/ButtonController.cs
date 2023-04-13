7b16becd526c7f5ba7f8e3accff28612417725e6
JacquelineHeisel3641 <117841731+JacquelineHeisel3641@users.noreply.github.com>
2023-04-12 22:00:54 -0500

    Quit Labeled

--- PlaceholderName/Assets/Scripts/ButtonController.cs
+++ PlaceholderName/Assets/Scripts/ButtonController.cs
@@ -43,6 +43,7 @@ public class ButtonController : MonoBehaviour
         //}
         // SceneManager.LoadScene("Quit Screen");
         Application.Quit();
+        Debug.Log("bye bye");
     }
 
     /// <summary>
