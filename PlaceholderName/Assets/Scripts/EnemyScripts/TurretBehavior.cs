10980d80b84693a241b5ed235d74c9765917e908
JBBateman <jbateman2@mail.bradley.edu>
2023-04-12 21:54:15 -0600

    Not even sure anymore

--- PlaceholderName/Assets/Scripts/EnemyScripts/TurretBehavior.cs
+++ PlaceholderName/Assets/Scripts/EnemyScripts/TurretBehavior.cs
@@ -1,3 +1,11 @@
+/*****************************************************************************
+// File Name :         TurretBehavior.cs
+// Author :            Jacob Bateman
+// Creation Date :     April 12, 2023
+//
+// Brief Description : Decides which player the turret will target, then fires 
+// bullets.
+*****************************************************************************/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,11 +22,16 @@ public class TurretBehavior : MonoBehaviour
     [SerializeField] private float rotationModifier;
     private float followDecider;
     [SerializeField] private float laserSpeed;
+    [SerializeField] private float fireRate;
 
     private bool followingPlayer1;
 
     private Transform laserSpawnPos;
 
+    /// <summary>
+    /// Decides which player the turret will target, invokes the command to fire 
+    /// bullets.
+    /// </summary>
     private void Start()
     {
         //Decides which player the enemy will initially follow.
@@ -26,6 +39,7 @@ public class TurretBehavior : MonoBehaviour
 
         AssignPlayers();
 
+        //Decides which player will be targeted.
         if (followDecider >= 0 && followDecider < 1)
         {
             followingPlayer1 = true;
@@ -35,40 +49,50 @@ public class TurretBehavior : MonoBehaviour
             followingPlayer1 = false;
         }
 
-        InvokeRepeating("FireBullets", 0f, 0.5f);
+        InvokeRepeating("FireBullets", 0f, fireRate);
     }
 
+    /// <summary>
+    /// Rotates the turret based off of the location of the player it is targeting.
+    /// </summary>
     private void FixedUpdate()
     {
         if (followingPlayer1)
         {
+            //Gets a position to target.
             Vector3 vectorToTarget = player1.transform.position - transform.position;
 
+            //Calculates the angle to rotate.
             float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg - rotationModifier;
 
+            //Rotates.
             Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
-
             transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * speed);
         }
         else
         {
+            //Gets a position to target.
             Vector3 vectorToTarget = player2.transform.position - transform.position;
 
+            //Calculates the angle to rotate.
             float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg - rotationModifier;
 
+            //Rotates.
             Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
-
             transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * speed);
         }
 
         laserSpawnPos = laserSpawn.transform;
     }
 
+    /// <summary>
+    /// Fires bullets.
+    /// </summary>
     private void FireBullets()
     {
-        //Bullet spawns at the location of the bulletSpawn GameObject, which is a
-        //child of the player. The direction it travels is determined by the
-        //rotation of the player.
+        //Bullet spawns at the location of the laserSpawn GameObject, which is a
+        //child of the turret. The direction it travels is determined by the
+        //rotation of the turret.
         var spawnedBullet = Instantiate(turretLaser, laserSpawn.transform.position,
             laserSpawnPos.rotation);
         spawnedBullet.GetComponent<Rigidbody2D>().velocity = laserSpawnPos.up
--- PlaceholderName/Assets/Scripts/PlayerAssignerController.cs
+++ PlaceholderName/Assets/Scripts/PlayerAssignerController.cs
@@ -1,3 +1,10 @@
+/*****************************************************************************
+// File Name :         PlayerAssignerController.cs
+// Author :            Jacob Bateman
+// Creation Date :     April 11, 2023
+//
+// Brief Description : Assigns boolean to determine whether the player is player 2.
+*****************************************************************************/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +13,9 @@ public class PlayerAssignerController : MonoBehaviour
 {
     public bool isPlayer2 = false;
 
+    /// <summary>
+    /// Sets isPlayer2 to true once player 1 has spawned in.
+    /// </summary>
     public void AssignBoolean()
     {
         if(isPlayer2 == false)
